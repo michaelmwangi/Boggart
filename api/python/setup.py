2b8b9fab559273d56d9f4e6f19ff503344c555ea
michael <michaelmwangi@operamail.com>
2017-03-12 11:18:43 +0000

    Added desc to installer

--- api/python/setup.py
+++ api/python/setup.py
@@ -1,7 +1,11 @@
+"""
+Boggart worker API installer
+"""
 from distutils.core import setup
 
 setup(
     name="BoggartWorkerApi",
+    description="python boggart worker API",
     version="0.1",
     author="michael mwangi",
     license="MIT",
